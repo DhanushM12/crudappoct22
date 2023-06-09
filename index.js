99e4e50a73b6ea4e085172873fece2e3a034a07e
DhanushM12 <dhanushmukhi@gmail.com>
2023-06-09 19:29:56 +0530

    added get and create

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const port = 8080;
 
 const db = require('./config/mongoose')
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('<h2>REST APIs for CRUD App</h2>')
+})
+
+const studentRouter = require('./routes/index')
+
+app.use('/student', studentRouter)
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server : ${err}`);
---
+++ routes/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+
+const Student = require('../models/student');
+
+// create a student
+router.post('/create', async (req, res) => {
+    const student = new Student({
+      name: req.body.name,
+      company: req.body.company  
+    })
+    try {
+        const newStudent = await student.save();
+        res.status(201).json({newStudent})
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+// get all data
+router.get('/list', async (req, res) => {
+    try {
+        const students = await Student.find();
+        res.status(200).json(students)
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+})
+
+router.get('/:id', getStudentDetails, async (req, res) => {
+    res.status(200).json(res.student)
+})
+
+async function getStudentDetails(req, res, next){
+    let student;
+    try {
+        student = await Student.findById(req.params.id)
+        if(student == null){
+            return res.status(404).json({message: "Cannot find student"})
+        }
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+    res.student =student;
+    next();
+}
+
+module.exports = router;
